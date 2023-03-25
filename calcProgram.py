6bbbac2ba5bc8bbd21888a2ef895e0c8be80c29b
emperorraccoonthe1st <owenmlopez07@gmail.com>
2023-03-24 22:23:12 -0400

    yes

--- calcProgram.py
+++ calcProgram.py
@@ -10,7 +10,7 @@ num2 = ""
 operatorValue = ""
 def putOne():
     global num1, num2, result
-    print(num1Inputed)
+    # print(num1Inputed)
     if num1Inputed == False:
         num1 = num1 + "1"
         print(num1)
@@ -34,13 +34,13 @@ def putZero():
 
 def putOne():
     global num1, num2, result
-    print(num1Inputed)
+    #print(num1Inputed)
     if num1Inputed == False:
         num1 = num1 + "1"
-        print(num1)
+        print("num1", num1)
     else:
         num2 = num2 + "1"
-        print(num2)
+        print("num2", num2)
     result = ""
 
 def putTwo():
@@ -150,7 +150,29 @@ def putDot():
             num2 = num2 + "."
         print(num2)
     result = ""
-  
+
+
+def putFlip():
+    global num1, num2, num1Inputed
+    if num1Inputed == False:
+        print(float(num1))
+        if float(num1) > 0:
+            print(num1, "bigger")
+            num1 = "-" + num1
+            print(num1)
+        else:
+            print(num1, "smaller")
+            num1 = num1.lstrip(num1[0])
+            print(num1)
+    else:
+        if float(num2) > 0:
+            print(num2, "bigger")
+            num2 = "-" + num2
+            print(num2)
+        else:
+            print(num2, "smaller")
+            num2 = num2.lstrip(num2[0])
+            print(num2)
           
 def addition():
     global num1, operatorValue, num1Inputed
@@ -173,11 +195,6 @@ def equationFinish():
 
 
 
-putOne()
-putTwo()
-putDot()
-putOne()
-
 
 
 
@@ -185,14 +202,47 @@ putOne()
 
 # Interface
 
+# 0
+btn0 = Button(root, text="0", command = putZero, width=5, height=1).place(anchor=CENTER, relx=0.1, rely=0.4)
+
 # 1
-btn1 = Button(root, text="1", command = putOne, width=5, height=1).place(anchor=CENTER, relx=0.1, rely=0.6)
+btn1 = Button(root, text="1", command = putOne, width=5, height=1).place(anchor=CENTER, relx=0.1, rely=0.1)
+
+# 2
+btn2 = Button(root, text="2", command = putTwo, width=5, height=1).place(anchor=CENTER, relx=0.2, rely=0.1)
+
+# 3
+btn3 = Button(root, text="3", command = putThree, width=5, height=1).place(anchor=CENTER, relx=0.3, rely=0.1)
+
+# 4
+btn4 = Button(root, text="4", command = putFour, width=5, height=1).place(anchor=CENTER, relx=0.1, rely=0.2)
+
+# 5
+btn5 = Button(root, text="5", command = putFive, width=5, height=1).place(anchor=CENTER, relx=0.2, rely=0.2)
+
+# 6
+btn6 = Button(root, text="6", command = putSix, width=5, height=1).place(anchor=CENTER, relx=0.3, rely=0.2)
+
+# 7
+btn7 = Button(root, text="7", command = putSeven, width=5, height=1).place(anchor=CENTER, relx=0.1, rely=0.3)
+
+# 8
+btn8 = Button(root, text="8", command = putEight, width=5, height=1).place(anchor=CENTER, relx=0.2, rely=0.3)
+
+# 9
+btn9 = Button(root, text="9", command = putNine, width=5, height=1).place(anchor=CENTER, relx=0.3, rely=0.3)
+
+# Dot
+btnDot = Button(root, text=".", command = putDot, width=5, height=1).place(anchor=CENTER, relx=0.2, rely=0.4)
+
+# Flip
+btnFlip = Button(root, text="+/-", command = putFlip, width=5, height=1).place(anchor=CENTER, relx=0.3, rely=0.4)
 
 # Addition
-btnAdd = Button(root, text="+", command = addition, width=5, height=1).place(anchor=CENTER, relx=0.5, rely=0.6)
+btnAdd = Button(root, text="+", command = addition, width=5, height=1).place(anchor=CENTER, relx=0.4, rely=0.1)
 
 # Equal
-btnAdd = Button(root, text="=", command = equationFinish, width=5, height=1).place(anchor=CENTER, relx=0.7, rely=0.6)
+btnAdd = Button(root, text="=", command = equationFinish, width=5, height=1).place(anchor=CENTER, relx=0.4, rely=0.4)
 
 
 
